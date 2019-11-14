b95a64d661d255a1f65e8862ed8f130f3c507220
Rodrigo Neves <rd_neves_dark@hotmail.com>
2019-11-13 22:16:00 -0400

    aula05

---
+++ src/java/dao/ClienteDAOImpl.java
@@ -0,0 +1,48 @@
+package dao;
+import entidades.Cliente;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+
+public class ClienteDAOImpl implements ClienteDAO{
+    
+    private EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("ProjetoPU");
+    
+    public void save( Cliente cliente){
+        EntityManager em = fabrica.createEntityManager();
+        em.getTransaction().begin();
+        if(cliente.getId()>0){
+            em.merge(cliente);
+        }else{
+            em.persist(cliente);
+        }
+        em.getTransaction().commit();
+        em.close();
+    }
+    
+    public void delete(Cliente cliente){
+        EntityManager em = fabrica.createEntityManager();
+        em.getTransaction().begin();
+        
+        if(!em.contains(cliente)){
+            cliente = em.merge(cliente);
+        }
+        em.remove(cliente);
+        
+        em.getTransaction().commit();
+        em.close();
+    }
+    public Cliente find(int id){
+        EntityManager em= fabrica.createEntityManager();
+        return em.find(Cliente.class, id);
+    }
+    
+    public List<Cliente> list(){
+        EntityManager em = fabrica.createEntityManager();
+        Query q = em.createQuery("SELECT c FROM Cliente as c ORDER BY c.id");
+        return q.getResultList();
+        
+    }
+}
